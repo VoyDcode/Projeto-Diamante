7213053e9a45a2a6e3fd38b8f23594efe8f5610f
VoyDcode <Victor_r.lima@hotmail.com>
2025-09-24 22:00:50 -0300

    Criação das respectivas pastas

--- bank-api/src/main/java/com/fiap/bank_api/BankApiApplication.java
+++ bank-api/src/main/java/com/fiap/bank_api/BankApiApplication.java
@@ -2,12 +2,20 @@ package com.fiap.bank_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BankApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiApplication.class, args);
+    }
 
+    @RestController
+    class RootController {
+        @GetMapping("/")
+        public String home() {
+            return "Projeto Bank - Victor Rodrigues e Renato Silva Alexandre Bezerra"; 
+    }
 }
