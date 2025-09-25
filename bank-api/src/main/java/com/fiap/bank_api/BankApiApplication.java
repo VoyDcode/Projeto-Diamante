9c2d31ea45ebfb1fc7bbe8a290bccdb134fb85e0
Renato-005 <rbezerra979@gmail.com>
2025-09-24 23:35:33 -0300

    ajuste de linha

--- bank-api/src/main/java/com/fiap/bank_api/BankApiApplication.java
+++ bank-api/src/main/java/com/fiap/bank_api/BankApiApplication.java
@@ -1,5 +1,5 @@
 package com.fiap.bank_api;
-
+  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
